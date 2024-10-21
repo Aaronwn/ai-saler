4f87e2213dccf85a06fb0456ff529453d2302bd4
wangneng <wangneng@51talk.com>
2024-10-21 15:29:35 +0800

    feat: 线上api

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,32 +1,37 @@
 import axios, { AxiosInstance, AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
+const isProduction = import.meta.env.PROD;
+
 const service: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_APP_BASE_API || '', // Remove '/api' here
-    timeout: 5000
+  baseURL: isProduction ? 'https://crmapi.51suyang.cn' : '',
+  timeout: 5000,
 });
 
 service.interceptors.request.use(
-    (config: InternalAxiosRequestConfig) => {
-        return config;
-    },
-    (error: AxiosError) => {
-        console.log(error);
-        return Promise.reject();
+  (config: InternalAxiosRequestConfig) => {
+    if (isProduction && config.url?.startsWith('/api')) {
+      config.url = config.url.replace('/api', '');
     }
+    return config;
+  },
+  (error: AxiosError) => {
+    console.log(error);
+    return Promise.reject();
+  }
 );
 
 service.interceptors.response.use(
-    (response: AxiosResponse) => {
-        if (response.status === 200) {
-            return response;
-        } else {
-            Promise.reject();
-        }
-    },
-    (error: AxiosError) => {
-        console.log(error);
-        return Promise.reject();
+  (response: AxiosResponse) => {
+    if (response.status === 200) {
+      return response;
+    } else {
+      Promise.reject();
     }
+  },
+  (error: AxiosError) => {
+    console.log(error);
+    return Promise.reject();
+  }
 );
 
 export default service;
